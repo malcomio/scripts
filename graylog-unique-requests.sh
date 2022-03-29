d4116f8fcf211da26b26abbe090fb5dc9b53eaf0
Malcolm Young <malcolm.young@capgemini.com>
2022-03-29 14:05:54 +0100

    use inline sed where possible

--- graylog-unique-requests.sh
+++ graylog-unique-requests.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
 # Analyse log output from Graylog to find unique Drupal paths.
+# On a Mac, this depends on gnu-sed: brew install gnu-sed
 
 echo "Removing the timestamp, source, and message columns."
-sed -E 's/\"(.*)\",\"(.*)\",\"(.*)\",\"(.*)\",\"(.*)\"/\"\4\",\"\5\"/' graylog-search-result-relative-604800.csv > output-1.csv
+sed -E 's/\"(.*)\",\"(.*)\",\"(.*)\",\"(.*)\",\"(.*)\"/\"\4\",\"\5\"/' graylog-search-result-relative-604800.csv > output.csv
 
 echo "Removing any trailing slashes"
-sed 's/\/\"/\"/g' output-1.csv > output-2.csv
+sed -i.bak 's/\/\"/\"/g' output.csv
 
 echo "Replacing any purely numeric sections with % signs"
-gsed -E 's/\/([0-9]+),/\/%,/g' output-2.csv > output-3.csv
-gsed -E 's/\/([0-9]+)\//\/%\//g' output-3.csv > output-4.csv
+gsed -E -i.bak 's/\/([0-9]+),/\/%,/g' output.csv
+gsed -E -i.bak 's/\/([0-9]+)\//\/%\//g' output.csv
 
 echo "Converting to lower case"
-tr '[:upper:]' '[:lower:]' < output-4.csv > output-5.csv
+tr '[:upper:]' '[:lower:]' < output.csv > output-1.csv
 
 echo "Filtering to only include unique paths"
-sort -u output-5.csv > unique-paths.csv
+sort -u output-1.csv > unique-paths.csv
 
 echo "Tidying up"
 rm output*.csv
+rm output*.csv.bak
 
